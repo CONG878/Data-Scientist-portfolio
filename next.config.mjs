abc0e80040638b799eca18bc2af4478730fb1e3d
CONG878 <98112875+CONG878@users.noreply.github.com>
2025-08-10 15:30:03 +0900

    Update next.config.mjs
    Github Pages 출력

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 정적 HTML 파일로 내보내기 설정
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  basePath: '/Data-Scientist-portfolio',
   images: {
-    unoptimized: true,
+    unoptimized: true, // next/image 최적화 비활성화 (정적 export 시 필요)
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
